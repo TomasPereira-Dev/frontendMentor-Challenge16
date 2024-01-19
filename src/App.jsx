0ae5c6671610f4555023a65a6fd019e6969bd4ce
Tomas Pereira <tomaspereira2k@gmail.com>
2024-01-19 20:26:47 -0300

    added filter by region + card design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon } from '@fortawesome/free-regular-svg-icons'
 import { faMagnifyingGlass, faChevronDown } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react'
+import { useState, useRef, useEffect, useCallback } from 'react'
+import axios from 'axios'
 
 const moonSvg = <FontAwesomeIcon icon={faMoon}/>
 const magnifyingGlassSvg = <FontAwesomeIcon icon={faMagnifyingGlass} />
@@ -10,13 +11,39 @@ const chevronDownSvg = <FontAwesomeIcon icon={faChevronDown} />
 
 function App() {
 
+  const [inputVal, setInputVal] = useState("") 
+  const [selectedRegion, setSelectedRegion] = useState("all")
+  const [countries, setCountries] = useState([])
   const [isOpen, setIsOpen] = useState(false)
-  const [selectedRegion, setSelectedRegion] = useState("All")
+  const inputRef = useRef(null)
+  
+  const mappedCountries = countries.map(country => 
+    <div className='max-w-72 bg-white rounded-md' key={`${country.ccn3}`}>
+      <img className='rounded-t-md' src={`${country.flags.svg}`} alt={`${country.flags.alt}`}/>
+      <div className='px-6 pt-6 pb-12'>
+        <p className='text-xl font-bold'>{country.name.common}</p>
+        <p className='text-sm'><span className='font-bold'>Population:</span> {country.population}</p>
+        <p className='text-sm'><span className='font-bold'>Region:</span> {country.region}</p>
+        <p className='text-sm'><span className='font-bold'>Capital:</span> {country.capital}</p>
+      </div>
+    </div>
+  )
 
   const menuHandler = () => {
     setIsOpen(!isOpen)
   }
 
+  useEffect(() => {
+    axios.get(`https://restcountries.com/v3.1/${selectedRegion}`)
+    .then((res) => {
+      setCountries(res.data)
+      
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [selectedRegion])
+  console.log(countries)
   return (
     <>
       <header className="flex justify-between items-center px-4 py-8 bg-white">
@@ -32,23 +59,26 @@ function App() {
             <div className='p-4 bg-white rounded-s-md cursor-pointer'>
               {magnifyingGlassSvg}
             </div>
-            <input className='p-4 w-full rounded-e-md' type="text" placeholder="Search for a country"/>
+            <input className='p-4 w-full rounded-e-md' ref={inputRef} onKeyDown={() => {setInputVal(inputRef.current.value)}} type="text" placeholder="Search for a country"/>
           </div>
-            <div className='w-fit'>
-              <div className='flex justify-between items-center gap-12 p-4 mt-10 bg-white rounded-md cursor-pointer' onClick={menuHandler}>
-                  <p>{selectedRegion !== "All" ? selectedRegion : "Filter by Region"}</p>
-                  {chevronDownSvg}
-              </div>
-              <div className={`${isOpen === true ? 'block': 'hidden'} bg-white rounded-md`}>
-                  <ul className='flex flex-col gap-2 p-4'>
-                    <li className='cursor-pointer' onClick={()=>{setSelectedRegion("Africa")}}>Africa</li>
-                    <li className='cursor-pointer' onClick={()=>{setSelectedRegion("America")}}>America</li>
-                    <li className='cursor-pointer' onClick={()=>{setSelectedRegion("Asia")}}>Asia</li>
-                    <li className='cursor-pointer' onClick={()=>{setSelectedRegion("Europe")}}>Europe</li>
-                    <li className='cursor-pointer' onClick={()=>{setSelectedRegion("Oceania")}}>Oceania</li>
-                  </ul>
-              </div>
+          <div className='w-fit'>
+            <div className='flex justify-between items-center gap-12 p-4 mt-10 bg-white rounded-md cursor-pointer' onClick={menuHandler}>
+                <p>Filter by Region</p>
+                {chevronDownSvg}
             </div>
+            <div className={`${isOpen === true ? 'block': 'hidden'} bg-white rounded-md`}>
+              <ul className='flex flex-col gap-2 p-4'>
+                <li className='cursor-pointer' onClick={()=>{setSelectedRegion("region/africa")}}>Africa</li>
+                <li className='cursor-pointer' onClick={()=>{setSelectedRegion("region/america")}}>America</li>
+                <li className='cursor-pointer' onClick={()=>{setSelectedRegion("region/asia")}}>Asia</li>
+                <li className='cursor-pointer' onClick={()=>{setSelectedRegion("region/europe")}}>Europe</li>
+                <li className='cursor-pointer' onClick={()=>{setSelectedRegion("region/oceania")}}>Oceania</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className='grid justify-center gap-10 mt-10'>
+          {mappedCountries}
         </div>
       </main>
     </>
