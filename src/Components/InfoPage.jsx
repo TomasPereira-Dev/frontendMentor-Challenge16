65d7491d9a28ed77b1e4fce2cc7e0ffc9c5bd307
Tomas Pereira <tomaspereira2k@gmail.com>
2024-01-24 05:44:30 -0300

    added navigation using the country's border buttons

--- src/Components/InfoPage.jsx
+++ src/Components/InfoPage.jsx
@@ -17,7 +17,8 @@ const InfoPage = ({ searchFilter, searchFilterHandler }) => {
 
     const mappedBorders = borderCountries ? borderCountries.map(border => 
         <div key={border}>
-            <a className="bg-white px-8 py-2">{border}</a>
+                <button className="bg-white px-8 py-2" onClick={()=> {searchFilterHandler(`https://restcountries.com/v3.1/alpha/${border}`)}}
+                 type="button">{border}</button>
         </div>
     ) : <p className="bg-white px-6 py-2">None</p>
 
@@ -61,7 +62,7 @@ const InfoPage = ({ searchFilter, searchFilterHandler }) => {
                     </ul>
                     <div>
                         <p className="text-lg font-semibold">Border Countries:</p>
-                        <div className="flex gap-4">
+                        <div className="grid grid-cols-3 gap-4 lg:flex lg:gap-4">
                             {mappedBorders}
                         </div>
                     </div>
